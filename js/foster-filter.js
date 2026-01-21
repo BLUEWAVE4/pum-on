6d1b4e8e4667ed18de07cd489b331c6d2bdd0e41
BLUEWAVE4 <dhkdwk7586@gmail.com>
2026-01-21 18:53:07 +0900

    수요일 마감

--- js/foster-filter.js
+++ js/foster-filter.js
@@ -1,9 +1,24 @@
-const fosterFilter = document.getElementsByClassName("filter-area");
-console.log(fosterFilter[0]);
+async function changeFilterArea() {
+    const fosterFilter = document.querySelector(".filter-area");
+
+    try {
+        fosterFilter.addEventListener("change", (e) => {
+            console.log(e.target.value);
+        });
+
+    } catch (error) {
+
+    }
+}
 
 
+/*
+    // 1. 필터 선택
+    // 2. 선택된 필터 결과 값 확인
 
-fosterFilter.addEventListener("change", (e) => {
-    console.log(e);
-    
-})
+    3. 선택한 필터 결과 값을 활용하여 조건에 맞는 데이터 불러오기
+    4. 데이터 개수 조회 > 데이터 길이에 맞게 페이지 분배
+        - 비활성화 페이지는 회색으로 표현
+        - 선택중인 페이지는 배경 표시
+    5. 
+*/
